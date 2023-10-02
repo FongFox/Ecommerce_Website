4aee077eb38667e462b5798e358c659f03ba3f46
FongFox <107380312+FongFox@users.noreply.github.com>
2023-10-03 00:08:13 +0700

    add front-end template (not done)

--- front-end/src/app/components/product-list/product-list.component.ts
+++ front-end/src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit{
