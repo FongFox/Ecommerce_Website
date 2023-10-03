dfe1fdad9aa8c24d18b4626999c9e70991d668d4
FongFox <107380312+FongFox@users.noreply.github.com>
2023-10-03 16:32:36 +0700

    update some stuff

--- back-end/src/main/java/com/ecommerce/springboot/repository/IProductDAO.java
+++ back-end/src/main/java/com/ecommerce/springboot/repository/IProductDAO.java
@@ -1,11 +1,15 @@
 package com.ecommerce.springboot.repository;
 
 import com.ecommerce.springboot.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
 @CrossOrigin
 public interface IProductDAO extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
--- front-end/src/app/components/product-list/product-list.component.ts
+++ front-end/src/app/components/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Routes } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
@@ -5,24 +6,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private ProductService: ProductService) { }
+  constructor(
+    private ProductService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.ProductService.getProductList().subscribe(
-      data => {
-        this.products = data;
-      }
-    );
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      //get the "id" param String, convert to string to number (by using "+" symbol)
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else {
+      //not category id available ... default to "undefine"
+      this.currentCategoryId = 1;
+    }
+
+    this.ProductService.getProductList(this.currentCategoryId).subscribe((data) => {
+      this.products = data;
+    });
   }
 }
